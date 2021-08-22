dd4dbd4d393a8cfea140afa105efaa112de00c8b
razer <humbertolucas.2416@aluno.saojudas.br>
2021-08-22 13:41:35 -0300

    incluindo arquivos faltantes do commint anterior

--- Empregado.java
+++ Empregado.java
@@ -33,5 +33,14 @@ public class Empregado{
  }
  public void setIdade (int idade){
  this.idade = idade;
+
+ public void setTipo (int tipo){
+    this.tipo = tipo;
+   }
+   public int tipo (){
+    return this.tipo;
+   }
+   
+
  }
 }
